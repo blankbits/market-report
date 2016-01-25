109adb3bd7b7f7b0fad44cc2924c03879e81df70
Peter Brandt <peterbrandt84@gmail.com>
2016-01-24 20:14:38 -0800

    Lots of comments and cleanup, now passes pylint.

--- text_utils.py
+++ text_utils.py
@@ -1,12 +1,44 @@
+# Copyright 2016 Peter Dymkar Brandt All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""This file contains util functions to assist in building text-based
+visualizations of numeric data.
+"""
+
 import numpy as np
 import pandas as pd
 
 def get_histogram(series, bins, bins_decimals=0, bins_is_percent=False,
                   block_count=100):
+    """Creates a text-based histogram.
+
+    Args:
+        series: pandas.Series of numeric values.
+        bins: List of boundaries between bins in ascending order.
+        bins_decimals: Number of decimals to use for bins in format string.
+        bins_is_percent: Whether to print a '%' character for bins.
+        block_count: Total number of block characters in histogram.
+    """
     histogram = ''
     buckets = series.groupby(pd.cut(series, bins)).count()
+
+    # Find the max string length for an individual bin value so that right
+    # alignment works properly.
     max_bin_value_len = len(str(int(np.round(max(abs(bins)))))) + (
         (bins_decimals + 1) if bins_decimals > 0 else 0) + 1
+
     format_str = '  '.join(['%+' + str(max_bin_value_len) + '.' + str(
         bins_decimals) + ('f%%'if bins_is_percent else 'f')] * 2) + (
             '  %-' + str(len(str(buckets.max()))) + 'i  %s\n')
@@ -14,14 +46,25 @@ def get_histogram(series, bins, bins_decimals=0, bins_is_percent=False,
         # Due to rounding exact number of blocks may vary.
         histogram += format_str % (bins[i], bins[i + 1], buckets[i], ''.join(
             [u'\u2588'] * np.round(block_count * buckets[i] / series.size)))
-
     return histogram
 
 def get_column(series, decimals=1, is_percent=False):
+    """Creates a text-based column with labels on the left and numeric values on
+    the right.
+
+    Args:
+        series: pandas.Series of numeric values.
+        decimals: Number of decimals to use in format string.
+        is_percent: Whether to print a '%' character for values.
+    """
     column = ''
+
+    # Find max string length for labels and values so that right alignment works
+    # properly.
     label_len = len(max(list(series.axes[0]), key=len))
     value_len = len(str(max([str(x) for x in np.round(
         list(abs(series.values)), decimals)], key=len))) + 1
+
     format_str = ('%-' + str(label_len) + 's  %+' + str(value_len) + '.' +
                   str(decimals) + 'f' + ('%%\n' if is_percent else '\n'))
     for key, value in series.iteritems():
@@ -29,8 +72,19 @@ def get_column(series, decimals=1, is_percent=False):
     return column
 
 def join_lines(columns, separator=''):
+    """Joins an arbitrary number of multi-line strings side-by-side splitting on
+    the '\n' character. It pads the ends of lines with ' ' characters as needed
+    to preserve alignment.
+
+    Args:
+        columns: List of multi-line strings to join.
+        separator: Padding inserted between each column.
+    """
     split_columns = [x.split('\n') for x in columns]
+
+    # Max line width for each input column.
     widths = [max(x) for x in [[len(y) for y in z] for z in split_columns]]
+
     result = ''
     for i in range(max([len(x) for x in split_columns])):
         for j, column in enumerate(split_columns):
--- universe_report.py
+++ universe_report.py
@@ -1,3 +1,31 @@
+# Copyright 2016 Peter Dymkar Brandt All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""UniverseReport generates text-based visualizations of past performance of a
+universe of financial instruments e.g. components of a market index.
+
+Example:
+    # See historical_data documentation for more info.
+    hd = historical_data.HistoricalData(historical_data_config,
+                                        tor_scraper_config)
+    daily = hd.get_daily()
+    if daily is None:
+        return
+    print universe_report.UniverseReport(daily).get_default_report()
+"""
+
 import sys
 
 import numpy as np
@@ -5,19 +33,37 @@ import numpy as np
 import text_utils
 
 class UniverseReport(object):
+    """Contains all functionality for the universe_report module.
+    """
     def __init__(self, daily):
+        """UniverseReport must be initialized with a pandas.DataFrame of prices
+        of the same type returned by historical_data.get_daily(). Rows represent
+        dates in descending order, and columns represent financial instruments.
+
+        Args:
+            daily: pandas.DataFrame containing historical price data.
+        """
         self._daily = daily
 
     def get_returns_report(self, offset, bins=None):
+        """Creates a multi-line string containing a column of top winners and
+        losers on the left, and a histogram of all returns on the right.
+
+        Args:
+            offset: Number of rows (days) back to go when calculating returns.
+            bins: List of boundaries between histogram bins in ascending order.
+        """
         returns = ((self._daily['price'].iloc[0, :] - (
             self._daily['price'].iloc[offset, :])) / (
                 self._daily['price'].iloc[offset, :]).sort_values())
 
+        # If no bins provided, create default of 20 equally spaced bins.
         if bins is None:
             bins = np.arange(returns.min() - sys.float_info.epsilon,
                              returns.max() + sys.float_info.epsilon,
                              .05 * (returns.max() - returns.min()))
         else:
+            # Possibly add bins at either end to ensure all values are counted.
             if returns.min() < bins.min():
                 bins = np.insert(bins, 0, returns.min() - (
                     sys.float_info.epsilon))
@@ -26,6 +72,9 @@ class UniverseReport(object):
 
         returns_hist = text_utils.get_histogram(100 * returns, 100 * bins, 0, (
             True))
+
+        # If there is space for winners and losers and at least one empty line
+        # separating them, then include them.
         extreme_count = int(np.floor((bins.size - 1) * .5)) - 1
         if extreme_count > 0:
             returns_col = text_utils.get_column(100 * returns[:extreme_count], (
@@ -40,28 +89,45 @@ class UniverseReport(object):
         return text_utils.join_lines([returns_col, returns_hist], '    ')
 
     def get_stats_report(self, offset, count):
+        """Creates a multi-line string with several columns of stats about the
+        universe for a given time period.
+
+        Args:
+            offset: Number of rows (days) back to go when calculating stats.
+                This must be at least 4 so that there are 2 periods to compare.
+            count: Number of values to include for volatility and volume.
+        """
+        # Prices with the most recent value at a max or min for the period.
         price_at_high = 'At High\n' + text_utils.get_column(
             self._daily['price'].ix[0, self._daily['price'].max(axis=0) == (
                 self._daily['price'].iloc[0, :])], 2)
         price_at_low = 'At Low\n' + text_utils.get_column(
             self._daily['price'].ix[0, self._daily['price'].min(axis=0) == (
                 self._daily['price'].iloc[0, :])], 2)
+
+        # Percent change in stdev from the first to second half of the period.
         volatility_change = self._daily['price'].iloc[range(
             offset / 2 + 1), :].std(axis=0) / (self._daily['price'].iloc[range(
                 (offset / 2), offset + 1), :].std(axis=0))
         volatility_change = 'Volatility Chg\n' + text_utils.get_column(
             100 * volatility_change.sort_values(ascending=False)[range(
                 count)], 2, True)
+
+        # Percent change in volume from the first to second half of the period.
         volume_change = self._daily['volume'].iloc[range(
             offset / 2 + 1), :].sum(axis=0) / (self._daily['volume'].iloc[range(
                 (offset / 2), offset + 1), :].sum(axis=0))
         volume_change = 'Volume Chg\n' + text_utils.get_column(
             100 * volume_change.sort_values(
                 ascending=False)[range(count)], 2, True)
+
         return text_utils.join_lines([price_at_high, price_at_low, (
             volatility_change), volume_change], '    ')
 
     def get_default_report(self):
+        """Creates a report including 1 and 20 day returns and 20 day stats. The
+        offsets and bin boundaries are hard-coded.
+        """
         result = '1 Day Returns\n-------------\n'
         result += self.get_returns_report(1, np.arange(-.2, .22, .02))
         result += '20 Day Returns\n--------------\n'
